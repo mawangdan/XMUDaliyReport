77023c39c3117bffdb0f899ce9127b337558dfa6
mawangdan <68174279+mawangdan@users.noreply.github.com>
2022-03-28 21:55:49 +0800

    Update vpnmain.py

--- src/vpnmain.py
+++ src/vpnmain.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 from functools import singledispatch
 
+import random
 import requests
 import re
 import execjs
@@ -143,7 +144,7 @@ def injectPersonalData(formDataJson, personalDataList):
 def MainFunction():
     global sendAddress, emailPsw, receiveAddress, username, psw, vpnPsw
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.3'+str(random.ranint(1,9)),
     }
     s = requests.session()
     # vpn登录
@@ -209,6 +210,7 @@ def MainFunction():
 if __name__ == '__main__':
     # 加载配置
     loadConfig()
+    time.sleep(random.ranint(0,5)*60)
     #20次重试次数
     for n in range(0,20):
         try:
